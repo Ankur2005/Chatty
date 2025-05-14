0e6d739de4e279386217aae367ffec4893a6c1d6
Ankur2005 <ankurbhatia938@gmail.com>
2025-05-14 23:14:57 +0530

    feat: initialize frontend with Vite and React setup
    - Added package.json for project dependencies and scripts.
    - Included Vite logo SVG in public directory.
    - Created App.css for styling the application.
    - Developed App.jsx as the main application component.
    - Added React logo SVG in assets directory.
    - Created index.css for global styles.
    - Implemented main.jsx for rendering the application.
    - Configured Vite with vite.config.js for React support.

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,113 @@
+import cloudinary from "../lib/cloudinary.js";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from 'bcryptjs';
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "Please fill all the fields" });
+        }
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "Email already exists" });
+        }
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const newUser = await User.create({
+            email,
+            fullName,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+            return res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePic: newUser.profilePic,
+            })
+        }
+        else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+
+    } catch (err) {
+        console.log("Error in signup controller: ", err.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).json({ message: "Please fill all the fields" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+
+        generateToken(user._id, res);
+        return res.status(200).json({
+            _id: user._id,
+            email: user.email,
+            fullName: user.fullName,
+            profilePic: user.profilePic,
+        })
+    } catch (error) {
+        console.log("Error in login controller: ", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+export const logout = (req, res) => {
+    try {
+        res.clearCookie("jwt", "", { maxAge: 0 });
+        return res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller: ", error.message);
+        res.status(500).json({ message: "Internal server error" });
+
+    }
+}
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id
+        if(!profilePic) {
+            return res.status(400).json({ message: "Please provide a profile picture" });
+        }
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            profilePic: uploadResponse.secure_url
+        }, { new: true });
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        console.log("Error in updateProfile controller: ", error.message);
+        res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
+
+export const checkAuth = (req, res) => {
+    try{
+        res.status(200).json(req.user);
+    }catch(error){
+        console.log("Error in checkAuth controller: ", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
