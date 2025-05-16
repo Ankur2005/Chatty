6c7c775d19081bb776679087ac461b3245d9a9d8
Ankur2005 <ankurbhatia938@gmail.com>
2025-05-17 01:04:47 +0530

    feat: add .gitignore, configure production settings, and update package dependencies
    //somewhat completed, will add new features

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,12 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
+import path from 'path';
 import { connectDB } from './lib/db.js';
 import {app,server} from './lib/socket.js';
 dotenv.config();
-app
 const PORT = process.env.PORT;
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -18,6 +18,12 @@ app.use(cors({
 }));
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    })
+}
 server.listen(PORT, ()=>{
     console.log("Server is running on port: "+PORT);
     connectDB();
